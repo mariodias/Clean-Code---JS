91512e6099cbb79ee09c901d44ea4c18b955d4a0
Mario Dias <mario.dias@ufpi.edu.br>
2024-12-30 16:41:35 -0300

    Respondendo o exercício: 3

--- desafios/03-causa-vs-efeito.tsx
+++ desafios/03-causa-vs-efeito.tsx
@@ -18,24 +18,24 @@ function fetchUser() {
 }
 
 export function UserProfile() {
-  const [shouldNotRenderUserName, setShouldNotRenderUserName] = useState(false)
+  const [isLoading, setIsloading] = useState(false)
   const [userData, setUserData] = useState<User>()
 
   useEffect(() => {
     function loadUser() {
-      setShouldNotRenderUserName(true)
+      setIsloading(true)
 
       const fetchUserResponse = fetchUser()
 
       setUserData(fetchUserResponse.data.user)
       
-      setShouldNotRenderUserName(false)
+      setIsloading(false)
     }
 
     loadUser()
   })
 
-  if (shouldNotRenderUserName) {
+  if (isLoading) {
     return <p>Loading...</p>
   }
 
